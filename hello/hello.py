bd341468ab7e0735522e5b802ec8f5cd2906af75
Aimal Khan <Aimal.khan@bryanuniversity.edu>
2021-11-22 23:11:03 -0500

    week-1

---
+++ hello/hello.py
@@ -0,0 +1,10 @@
+msg = "Hello World"
+print(msg)
+
+msg
+msg.capitalize()
+'hello world'
+print(msg)
+msg.split()
+['Hello','World']
+print(msg)
---
+++ practice/index1.py
@@ -0,0 +1,5 @@
+name = input("tell me your name: Aimal")
+print("your name is " + name)
+
+age = input("tell me your age: 29")
+print("your age is " + age)
